c61d710b1fd7e5f533bf5433425c0234550a7a45
Jack Tsang <cic_tian2@yahoo.com>
2019-10-18 13:44:45 -0700

    Added index.html file
    index.js loads from index.html instead of a URL.

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const { BrowserWindow, app } = require('electron');
 
 function createWindows(){
     let appWindow = new BrowserWindow({width: 800, height: 600, frame: false});
-    appWindow.loadURL('https://7ty.tech');
+    appWindow.loadFile('index.html');
 }
 
 app.on('ready', createWindows);
