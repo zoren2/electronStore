20d5b2df1d55b4388f390ca3bce9ce794a0ea177
Jack Tsang <cic_tian2@yahoo.com>
2019-10-18 14:24:34 -0700

    Edited BrowserWindow default behavior

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 const { BrowserWindow, app } = require('electron');
 
 function createWindows(){
-    let appWindow = new BrowserWindow({width: 800, height: 600, frame: false});
+    let appWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        center: true,
+        minWidth: 300,
+        minHeight: 400,
+        frame: false,
+    });
     appWindow.loadFile('index.html');
 }
 
